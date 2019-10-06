6a605de51fae8cef6e936335944fe872db5c2bf0
hoodielive <hoodielive@gmail.com>
2019-10-06 19:38:14 -0400

    dopeness

--- uncle/tariq.php
+++ uncle/tariq.php
@@ -1,16 +1,18 @@
+<?php
+
 class ShopProduct 
 {
     public $title; 
     public $producerMainName; 
     public $producerFirstName; 
-    public $price 
+    public $price = 0;
 
-    public function __construct(
-        $title,
+    public function __construct( 
+        $title, 
         $firstName, 
         $mainName, 
         $price
-    ) {
+    ){
         $this->title = $title; 
         $this->producerFirstName = $firstName; 
         $this->producerMainName = $mainName;
