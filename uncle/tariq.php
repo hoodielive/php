bcc725da78caf61651f0e91132ebc86a0c5f9cb7
hoodielive <hoodielive@gmail.com>
2019-10-06 17:34:41 -0400

    php objects, patterns and practice

---
+++ uncle/tariq.php
@@ -0,0 +1,33 @@
+class ShopProduct 
+{
+    public $title; 
+    public $producerMainName; 
+    public $producerFirstName; 
+    public $price 
+
+    public function __construct(
+        $title,
+        $firstName, 
+        $mainName, 
+        $price
+    ) {
+        $this->title = $title; 
+        $this->producerFirstName = $firstName; 
+        $this->producerMainName = $mainName;
+        $this->price = $price; 
+    }
+
+    public function getProducer() 
+    {
+        return $this->producerFirstName . " " . $this->producerMainName;
+    }
+}
+
+$product1 = new ShopProduct(
+    "My Antonia",
+    "Willa",
+    "Cather", 
+    5.99
+);
+
+print "author: {$product1->getProducer()}\n"; 
