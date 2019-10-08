98e35552cd3e2e59136ed87ae1e8a451d08a810e
hoodielive <hoodielive@gmail.com>
2019-10-07 22:51:19 -0400

    <connector>

--- uncle/pull_from_db.php
+++ uncle/pull_from_db.php
@@ -2,12 +2,20 @@
 
 class Connector
 {
-	$user = "laradev";
-	$pass = "password"; 
-
-	public function connect($pdo)
+	public function connect()
 	{
-		$pdo = new PDO("mysql://127.0.0.1;dbname=regis", $user, $pass); 
+		$user = "laradev";
+		$pass = "password"; 
+		try 
+		{
+			return new PDO('mysql:host=127.0.0.1;dbname=regis', $user, $pass);
+		}
+			catch (PDOException $e)
+		{
+			die($e->getMessage());
+		}
 	}
 }
 
+$pdo = Connector::connect(); 
+var_dump($pdo);
