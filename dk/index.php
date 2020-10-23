f2122a5c3f0851b2b00035ba6ba77f9536aaf785
hoodielive <hoodielive@gmail.com>
2020-10-22 22:36:14 -0400

    class templates

---
+++ dk/index.php
@@ -0,0 +1,34 @@
+<?php 
+
+class Route
+{
+
+}
+
+class User
+{
+
+}
+
+class Invoice
+{
+	public $id;
+	public $date;
+}
+
+class InvoiceItem
+{
+
+}
+
+class AchievementBadge
+{
+	public $title;
+	public $description;
+	public $points;
+
+	public function awardTo($user)
+	{
+
+	}
+}
--- dk/php_class.php
+++ dk/php_class.php
@@ -2,14 +2,15 @@
 
 class ASampleClass
 {
-	public function __construct(
+	public function __construct (
 		@@CustomAttribute
 		public int $anInt = 0, 
 		/** @var float */
 		public float $aFloat,
 		float $anotherFloat, // NOT promoted, tricky.
 		public MyClass $objectOfMyClass,
-	) {
+	) 
+	{
 		// $anInt = $anInt < 0 ? 0 : $anInt;
 	}
 }
