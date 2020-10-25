ea2dafac9cc6307ef441ce1b15da2e0a10d8d78a
hoodielive <hoodielive@gmail.com>
2020-10-24 21:39:08 -0400

    Conditionals and \n

--- php8/conditionals.php
+++ php8/conditionals.php
@@ -5,11 +5,24 @@ $b = "B";
 
 if ( $a !== $b )
 {
-	print "$b and $a are not equal";
+	print "$b and $a are not equal" . "\n";
 }
 else 
 {
-	print "$b and $a are equal.";
+	print "$b and $a are equal." . "\n" ;
 }
 
+
+
 # The 2nd conditional.
+$a = 25.1;
+$b = 36;
+
+if ( $a < $b )
+{
+	print "$a is less than $b.";
+}
+else
+{
+	print "$b is greater than $a.";
+}
