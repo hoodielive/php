0e9efa482d82b41e0e4120a4f43186e7295e7485
hoodielive <hoodielive@gmail.com>
2020-10-24 23:42:46 -0400

    Trying to figure out how to add custom routes.

--- php8/laravellove/config/app.php
+++ php8/laravellove/config/app.php
@@ -229,4 +229,17 @@ return [
 
     ],
 
+    'debug_hide' => [
+        '_ENV' => [
+          'APP_KEY',
+          'DB_PASSWORD',
+				],
+				'_SERVER' => [
+					'APP_KEY',
+					'DB_PASSWORD',
+				],
+				'_POST' => [
+					'password',
+				],
+    ],
 ];
