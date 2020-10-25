1c30215cdcf3b0ce26a84b3b4bd782db099d84b6
hoodielive <hoodielive@gmail.com>
2020-10-24 21:26:57 -0400

    PHP 8

--- php8/arrays.php
+++ php8/arrays.php
@@ -3,6 +3,11 @@ $class_array = array(
 	"No Longer" => "Programming Concepts",
 	"Shorter Than Former" => "Programming",
 	"Alice in Wonderland" => "In wonder land... la la la",
+), 
+array(
+	"This is the Second Array" => "Funny watching witches",
+	"Waisting precious time" => "Lolololol",
+	"PHP 8 is Cool" => "Yes it is",
 );
 
 var_dump($class_array);
