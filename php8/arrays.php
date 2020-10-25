26f4d5bcfe29d786cec78f0ebdc087d7214de6e9
hoodielive <hoodielive@gmail.com>
2020-10-24 21:24:43 -0400

    PHP 8

---
+++ php8/arrays.php
@@ -0,0 +1,8 @@
+ <?php
+$class_array = array(
+	"No Longer" => "Programming Concepts",
+	"Shorter Than Former" => "Programming",
+	"Alice in Wonderland" => "In wonder land... la la la",
+);
+
+var_dump($class_array);
